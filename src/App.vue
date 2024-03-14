de8ee7922bc20bc3a507d3fe7582e9869d6ad50a
Paterne <angepaterne.dali@gmail.com>
2024-03-14 20:36:06 +0100

    ..

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+
+import home from './components/home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+
+*{
+  background:black;
+  margin:0;
+  padding:0
+}
+
+@font-face {
+  font-family: RedHat;
+  src: url(../fontFamily/Red_Hat_Display/RedHatDisplay-VariableFont_wght.ttf);
+}
+
+#app {
+ font-family: RedHat
+}
+</style>
