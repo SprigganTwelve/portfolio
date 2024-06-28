4b1534b1d1b2a3fc1d0c7ed79080532a98dc8e6d
Paterne <angepaterne.dali@gmail.com>
2024-06-28 09:55:35 +0200

    .

--- vue/src/components/largeCard.vue
+++ vue/src/components/largeCard.vue
@@ -1,82 +1,144 @@
 <template>
-    <div>
-      <div class="mainComponent">
-        <div style="height: 20px;"></div>
-        <div v-for="item in articles" :key="item.guid" class="rowLayout">
-            <img  :src="item.enclosure.link??'../../public/depositphotos_62706797-stock-illustration-document-flat-circle-icon-with.jpg'" :alt="item.title">
-            <div class="columnLayout">
-                <h2>{{ item.title }}</h2>
-                <p>{{ item.description}}</p>
-                <a style="text-align: start;" :href="item.link">En savoir plus</a>
+  <!-- present--->
+    <div id="largeCard">
+  
+          <div class="atEnd">
+            <div class="describe">
+              <h1>les technologies de communication par satellites </h1>
+              <div  style="height: 20px;"></div>
+              <p>
+                Cette étude explore les avancées récentes dans les technologies de communication par satellites, depuis les méthodes de communication entre satellites et appareils, en passant par les différentes catégories de satellites, jusqu'aux technologies émergentes et leurs applications. Elle examine également les perspectives d'avenir pour ce secteur en constante évolution, mettant en lumière son impact potentiel sur divers domaines.
+              </p>
+              <a href="../../public/satellite.docx" download="satellite.docx">   <svg :class="color" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg></a>
+              <div  style="width: 20px;"></div>
             </div>
-        </div>
+              <div>
+                  <img  src="../assets/images/satellite-meteorologique_Nasa_wiki_dp.jpg" alt="">
+                  <div class="downloadLink">
+                      <h1>Les satellite</h1>
+  
+                  </div>
+              </div>
+          </div>
+   
+          <div class="atFirst" >
+              <div >
+               
 
-      </div>
+              </div>
+          </div>
+  
     </div>
   </template>
   
   <script>
-  import axios from 'axios';
   
   export default {
     name: 'App5',
-    props: ['color'],
-    data() {
-      return {
-        articles: []
-      }
-    },
-    methods: {
-      async fetchRSSFeed() {
-        const RSS_JSON_URL = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fiatranshumanisme.com%2Ffeed%2F";
-  
-        try {
-          const response = await axios.get(RSS_JSON_URL);
-          this.articles = response.data.items.slice(0, 2) || [];
-          console.log(response);
-        } catch (error) {
-          console.error('Error fetching the RSS feed:', error);
-        }
-      }
-    },
-    mounted() {
-      this.fetchRSSFeed();
-    }
+    props: ['color']
   }
   </script>
   
   <style scoped>
-  .mainComponent {
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-    justify-content: center;
-    align-items: center;
+  /*----position---*/
+  
+  #largeCard {
+      display:flex;
+      flex-direction:column;
+      justify-content: center;
+      align-items: center;
+      gap: 30px;
+      padding:30px;
+      margin: 70px;
   }
   
-  .rowLayout {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
+  #largeCard  div > img {
+      border-radius: 30px;
   }
-
-  .columnLayout{
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    justify-content: start;
-    width: 700px;
-    gap: 20px;
+  
+  #largeCard > .atEnd {
+      width: 100%;
+      display: flex;
+      justify-content: end;
   }
-
-img{
-    height: 350px;
-    width: 400px;
-    border-radius: 20px;
-}
-
-p{
-    font-size: 20px;
-}
+  
+  #largeCard > .atFirst {
+      width: 100%;
+      display: flex;
+      justify-content: start;
+  }
+  
+  #largeCard > div >div > img {
+      object-fit:cover;
+      object-position:center;
+      width:700px;
+      height:330px;
+      border: none;
+  
+  }
+  
+  
+  #largeCard > div >div > span {
+      position:absolute;
+      z-index:9999;
+      bottom:5px;
+      left:30px;
+  }
+  
+  #largeCard > div> div {
+      position: relative;
+      width:700px;
+  }
+  
+  #largeCard > div> div > svg{
+      position:absolute;
+      z-index:9999;
+      bottom:5px;
+      right:5px;
+      transition: all 1.3s
+  }
+  
+  /*-----color---*/
+  
+  /*---- Svg -color---*/
+  
+  .iswhite{
+      fill:black;
+      background:white;
+      margin-left:30px;
+      padding:10px;
+      margin-top:20px;
+      border-radius:50%;
+      margin-bottom:15px;
+      cursor:pointer
+  }
+  .isblack {
+      fill:white;
+      background:black;
+      margin-left:30px;
+      padding:10px;
+      margin-top:20px;
+      border-radius:50%;
+      margin-bottom:15px;
+      cursor:pointer
+  }
+  
+  #largeCard > div > div>  svg:hover{
+   background:red;
+   fill:white
+  }
+  /*--------*/
+  
+  /*-----border card Color ---*/
+  
+  .atEnd >div  {
+  }
+  
+  .downloadLink{
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+  }
+  
+  
   </style>
-  
